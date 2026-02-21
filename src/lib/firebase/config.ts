a0ae6567fda1b2a2baa46d55198ad8beef1aea01
Dhvani Shah <dhvanihshah06@gmail.com>
2026-02-21 07:00:16 +0000

    try {   40 |       const authFunction = action === 'signIn' ? signIn : s

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -7,6 +7,7 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
