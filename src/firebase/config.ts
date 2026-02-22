825fb2e14cc458daf05db85a2d4f3aaa18f32519
Dhvani Shah <dhvanihshah06@gmail.com>
2026-02-22 13:00:35 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,11 +7,11 @@ export function getFirebaseConfig(): FirebaseOptions {
         projectId: "gharsathi-app-43431",
         storageBucket: "gharsathi-app-43431.appspot.com",
         messagingSenderId: "870624021289",
-        appId: "1:870624021289:web:53163e79029729910a26d1"
+        appId: "1:870624021289:web:53163e79029729910a26d1",
+        measurementId: "G-9XG183K90M"
     };
 
     if (!firebaseConfig.apiKey || !firebaseConfig.projectId || !firebaseConfig.appId) {
-        // This error should not be thrown anymore as the config is hardcoded.
         throw new Error('Firebase config is missing or invalid.');
     }
 
