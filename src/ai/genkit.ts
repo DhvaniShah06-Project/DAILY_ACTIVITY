5a30379090d659500fef517b3ed407d04a73a5e9
Dhvani Shah <dhvanihshah06@gmail.com>
2026-02-22 13:20:18 +0000

    fix error

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import { getFirebaseConfig } from '@/firebase/config';
+
+const firebaseConfig = getFirebaseConfig();
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: firebaseConfig.apiKey})],
   model: 'googleai/gemini-2.5-flash',
 });
